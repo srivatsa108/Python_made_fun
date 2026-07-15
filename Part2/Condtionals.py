35e1b0f979891aa469f02eff6563b98cb3e9d9bb
Srivatsa <vatsa0077@gmail.com>
2026-07-15 18:58:19 +0530

    conditional part 1

---
+++ Part2/Condtionals.py
@@ -0,0 +1,10 @@
+#Conditionals
+var1 = 10
+var2 = 20
+if var1 < var2:
+    print("var2 is greater than var1")
+
+var3=int(input("Enter a number: "))
+
+if var3 < var2:
+    print("var3 is less than var2")
