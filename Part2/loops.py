ee6b074a3f979c7cbfdd485a94f4a9e939a317a3
Srivatsa <vatsa0077@gmail.com>
2026-08-16 21:20:53 +0530

    new commit with updated comment

--- Part2/loops.py
+++ Part2/loops.py
@@ -8,6 +8,7 @@ dict1={"name":"John", "age":30, "city":"New York"}
 for key in dict1.keys():
     print(key)
  
+#while loop is similar to an if statement, but it will continue to loop until the condition is no longer true.
 val3=5   
 while val3>0:
     print(val3)
