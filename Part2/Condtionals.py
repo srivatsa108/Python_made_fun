8c32c059a721e007ab30f62bfef79e10ffcc91f4
Srivatsa <vatsa0077@gmail.com>
2026-08-16 21:07:50 +0530

    update new

--- Part2/Condtionals.py
+++ Part2/Condtionals.py
@@ -8,3 +8,21 @@ var3=int(input("Enter a number: "))
 
 if var3 < var2:
     print("var3 is less than var2")
+
+var4, var5 = 1, 2
+#Conditional operations are and or and not
+if var4==1 and var5==2:
+    print("Both conditions are true")
+    
+if var4==1 or var5==2:
+    print("At least one condition is true")
+    
+#else, elif and if statements
+
+if var3 < var2:
+    print("var3 is less than var2")
+elif var3 == var2:
+    print("var3 is equal to var2")
+else:
+    print("var3 is greater than var2")
+    
