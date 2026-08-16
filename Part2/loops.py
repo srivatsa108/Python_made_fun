69df440b35b53fd104c10a045de0c399e8ce6bbc
Srivatsa <vatsa0077@gmail.com>
2026-08-16 21:18:56 +0530

    new committed

---
+++ Part2/loops.py
@@ -0,0 +1,14 @@
+#for and while loop
+
+print("The range(0,5)")
+for number in range(0,5):
+    print(number)
+
+dict1={"name":"John", "age":30, "city":"New York"}
+for key in dict1.keys():
+    print(key)
+ 
+val3=5   
+while val3>0:
+    print(val3)
+    val3 -= 1
