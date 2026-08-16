d27609b712b6845d00e0202994a8756ad98336a8
Srivatsa <vatsa0077@gmail.com>
2026-08-16 22:02:38 +0530

    comment updated

--- Part3/filehandling.py
+++ Part3/filehandling.py
@@ -1,7 +1,8 @@
 #File Handling code
 #The default mode in file handling is read only mode. If we want to write to a file, we need to open it in write mode or append mode.
+# To execute  change the directory to the location of the file and run the command "python filehandling.py" in the terminal.
 
-handle=open("sample.txt", "r")  # Open the file in read mode
+handle=open("sample.txt","r")  # Open the file in read mode
 data=handle.read()  # Read the contents of the file
 print(data)  # Print the contents of the file
 handle.close()  # Close the file
