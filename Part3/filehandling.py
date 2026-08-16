f134812269eceeac3c3efa30c315616655d708b9
Srivatsa <vatsa0077@gmail.com>
2026-08-16 22:11:16 +0530

    try except updated

--- Part3/filehandling.py
+++ Part3/filehandling.py
@@ -6,3 +6,15 @@ handle=open("sample.txt","r")  # Open the file in read mode
 data=handle.read()  # Read the contents of the file
 print(data)  # Print the contents of the file
 handle.close()  # Close the file
+
+#readline() will read line by line and readlines() will read all the lines.
+
+'''Use with keyword to open files. 
+Creates a context manager to automatically close the file after the block of code is executed.''' 
+
+try:
+    with open("sample.txt") as handle:
+        for line in handle:
+            print(line)
+except IOError:
+    print("File not found or path is incorrect")
